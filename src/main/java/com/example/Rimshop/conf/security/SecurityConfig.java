140ca6cab2c0111a9bab62c6da1b6e9cea5afcf4
renaon <egorbaturin2013@yandex.ru>
2022-09-28 17:44:01 +0300

    Время авторизации

--- src/main/java/com/example/Rimshop/conf/security/SecurityConfig.java
+++ src/main/java/com/example/Rimshop/conf/security/SecurityConfig.java
@@ -1,14 +1,17 @@
 package com.example.Rimshop.conf.security;
 
 import com.example.Rimshop.service.UserSevice;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
@@ -17,20 +20,33 @@ import javax.sql.DataSource;
 
 @Configuration
 @EnableConfigurationProperties
+@RequiredArgsConstructor
+@Slf4j
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     UserSevice userSevice;
 
+//    private JwtRequestFilter jwtRequestFilter;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable();
-        http.authorizeRequests()
-                .antMatchers("/admin/**").hasRole("admin")
-                .antMatchers("/user/**").hasAnyRole("client", "admin")
-                .antMatchers("/addform.html").hasRole("admin")
+//        http.cors().and().csrf().disable();
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/admin/**").hasAuthority("admin")
+                .antMatchers("/user/**").hasAnyAuthority("client", "admin")
+                .antMatchers("**/addform.html").hasAuthority("admin")
                 .antMatchers("/cart/**").permitAll()
                 .antMatchers("/**").permitAll()
-                .and().formLogin();
+                .anyRequest().permitAll()
+                .and().httpBasic()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .headers().frameOptions().disable()
+                .and().formLogin()
+                .defaultSuccessUrl("/admin");
     }
 
     @Override
@@ -44,8 +60,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+//    @Bean
+//    public JdbcUserDetailsManager userDetailsManager(DataSource dataSource) {
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
+
+    @Override
     @Bean
-    public JdbcUserDetailsManager userDetailsManager(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
+
